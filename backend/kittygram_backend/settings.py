91a35592ca2ea8321e428f768f969c13a0766284
Viva <sovestyaaaa@gmail.com>
2023-08-08 14:26:15 +0000

    Review 1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.70.236', '127.0.0.1', 'localhost', 'kittygram-lavita.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -90,10 +90,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend' 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/kittygram/media'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
